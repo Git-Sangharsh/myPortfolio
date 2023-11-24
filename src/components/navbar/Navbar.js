639981ad3448bb2379ef936f5bb65541df41211d
sangharsh <sotumotghare@gmail.com>
2023-11-24 17:00:35 +0530

    Added react-scroll to content className

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,23 +1,25 @@
 import React from "react";
 import "./Nav.css";
 import { Link } from "react-scroll";
-import logo from '../assets/logo.png'
+import logo from "../assets/logo.png";
 const Navbar = () => {
   return (
     <div className="nav">
       <div className="nav-wrapper">
         <div className="nav-left">
-          <img src={logo} className="logo" alt="" />
+          <Link to="content" spy={true} smooth={true} duration={2000}>
+            <img src={logo} className="logo" alt="" />
+          </Link>
         </div>
         <div className="nav-right">
           {/* <h1 className="headers">Skills</h1> */}
-          <Link to="do"  spy={true} smooth={true} duration={1000}>
+          <Link to="do" spy={true} smooth={true} duration={1000}>
             <h1 className="headers">About</h1>
           </Link>
-          <Link to="skills"  spy={true} smooth={true} duration={1000}>
+          <Link to="skills" spy={true} smooth={true} duration={1000}>
             <h1 className="headers">Project</h1>
           </Link>
-          <Link to="Contact"  spy={true} smooth={true} duration={1000}>
+          <Link to="Contact" spy={true} smooth={true} duration={1000}>
             <h1 className="headers border">Contact</h1>
           </Link>
         </div>
