e8f18b588047e3f7c89e0326552a4bda181ea71b
sangharsh <sotumotghare@gmail.com>
2023-11-26 00:09:54 +0530

    added react-parallax to components

--- src/App.js
+++ src/App.js
@@ -7,15 +7,20 @@ import Skills from "./components/skills/Skills";
 import Do from "./components/do/Do";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
+import { Parallax } from "react-parallax";
+import logo69 from "./components/assets/logo3.jpg";
 const App = () => {
   return (
-    <div className="App" >
+    <div className="App">
       <Helmet>
         <title>Sangharsh Portfolio</title>
       </Helmet>
       <Navbar />
-      <Content />
+      <Parallax strength={500} bgImage={logo69} >
+        <Content />
+      </Parallax>
       <Do />
+
       <Skills />
       <Contact />
       <Footer />
